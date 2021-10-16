f5a0c9d08537b4b6f6014a3cd2228aa767185aa6
FillZpp <FillZpp.pub@gmail.com>
2021-10-16 16:03:29 +0800

    Optimize CI build
    Signed-off-by: FillZpp <FillZpp.pub@gmail.com>

--- scripts/generate_protobuf.sh
+++ scripts/generate_protobuf.sh
@@ -12,11 +12,13 @@ fi
 
 set -e
 TMP_DIR=$(mktemp -d)
+mkdir -p "${TMP_DIR}"/bin
 mkdir -p "${TMP_DIR}"/src/github.com/openkruise/controllermesh
 cp -r ./{apis,hack,vendor} "${TMP_DIR}"/src/github.com/openkruise/controllermesh/
 
 (cd "${TMP_DIR}"/src/github.com/openkruise/controllermesh; \
-    GOPATH=${TMP_DIR} go install github.com/openkruise/controllermesh/vendor/k8s.io/code-generator/cmd/go-to-protobuf/protoc-gen-gogo; \
+    GO111MODULE=off GOPATH=${TMP_DIR} go build  -o ${TMP_DIR}/bin/protoc-gen-gogo github.com/openkruise/controllermesh/vendor/k8s.io/code-generator/cmd/go-to-protobuf/protoc-gen-gogo;
+    #GOPATH=${TMP_DIR} go install github.com/openkruise/controllermesh/vendor/k8s.io/code-generator/cmd/go-to-protobuf/protoc-gen-gogo; \
     PATH=${TMP_DIR}/bin:$PATH GOPATH=${TMP_DIR} protoc --gogo_out=plugins=grpc:. apis/ctrlmesh/proto/ctrlmesh.proto)
 
 cp -f "${TMP_DIR}"/src/github.com/openkruise/controllermesh/apis/ctrlmesh/proto/*.go apis/ctrlmesh/proto/
